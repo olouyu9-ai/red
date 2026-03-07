5d632ec13dbe67b8905de8b83e658a1d55536e1e
joseph <ngalaj33@gmail.com.com>
2026-03-07 23:02:10 +0100

    kddf

--- plateforme_parrainage/applications/comptes/urls.py
+++ plateforme_parrainage/applications/comptes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pyramid0001/', views.vue_inscription, name='inscription'),
+    path('ramid0001/', views.vue_inscription, name='inscription'),
     path('pyramid0002/', views.vue_ajouter_code_parrain, name='ajouter_parrain'),
     path('pyramid0003/', views.profile_view, name='profile'),
     path('pyramid0004/', views.profile_edit, name='profile_edit'),
